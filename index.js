f7f9d8ccd169bf94c7c28e3fb3e62ac86579af01
Benah0987 <88454033+Benah0987@users.noreply.github.com>
2023-03-20 15:53:51 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+// function introduction(name) {
+//     console.log(`Hi, my name is ${name}.`);
+// }
+// introduction("Benah")
+
+function introductionWithLanguageOptional(name, language= "JavaScript") {
+    console.log(`Hi, my name is ${name}, learning ${language}`);
+    
+} 
+
+introductionWithLanguageOptional("Benah", "JavaScript");
